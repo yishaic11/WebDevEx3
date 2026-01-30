5260308845ac452be9d88953aded94787aecb1a7
yishaic11 <yishai.chen28@gmail.com>
2026-01-30 16:32:53 +0200

    Work in Progress

---
+++ src/components/GamePageHeader.tsx
@@ -0,0 +1,9 @@
+const GamePageHeader = () => {
+  return (
+    <header className="mb-4">
+      <h1 className="text-center">Tic-Tac-Toe</h1>
+    </header>
+  );
+};
+
+export default GamePageHeader;
